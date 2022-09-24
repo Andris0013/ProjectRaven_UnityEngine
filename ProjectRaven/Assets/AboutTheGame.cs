38802e1ad95c652fe446171bb89e1f56fa4c6e89
Andris0013 <andris.madacsi@gmail.com>
2022-09-24 15:36:20 +0200

    FinalVersion - 24/09/22 - MA

---
+++ ProjectRaven/Assets/AboutTheGame.cs
@@ -0,0 +1,25 @@
+/*      Explaining the game and the creation process
+
+1. The concept
+    - You have a 3rd person character with a katana and have to go through the level destroying multiple enemy objects. These are basic objects that kill you if they manage to touch you so you need to hit them first.
+    - Every fight is a one hit interaction when you either win or die
+    - If you die you start again
+    - If you win you move forward
+    - There is an enemy object that needs to be killed in order to win the game, the goal is to try to find it. Once you destroy this object you win.
+
+2. Behind the scenes
+    - I used cinemachine fo the 3rd person camera because I heard good things about it from youtube and from the unity community. It worked I think however I did not have time to experiment with a lock on feature
+    - Assets from the unity store are all free assets: Katana, Fantasy forest, melee character animations
+    - Enemies are basic game objects, however I started experimenting with enemy characters and made the script with a humanoid enemy in mind (with option for multiple hit combat).
+    - For character movement I used the built in character controller because it seemed easier to apply for this game and I also programmed Gravity so it is more realistic,
+      however I do not use it yet. I did not have time to implement jump and sprint functions but I left the option to be able to in the future.
+    - I also planned on implementing loot that enemies drop when they die. We learned everything in class to be able to do it so it shouldn't be that hard, however I wanted to focus more on the feel of the game and basic functions that make the game work properly
+
+3. The future of the game
+      
+    I plan on expanding the game, changing a lot of things. I think I was able to make a solid base with the character mover and run into issues that made me realize what I need to improve and concentrate better in the future. What I liked the most is 
+creating the level and using unity assets to try and make the game look good and enjoyable. I would like to do that a lot in the future because writing the code took away a lot of the projects time. It is important though because I also want to make in-
+teractable objects as well. My goal is to slowly build a game with solid features that players can enjoy both because of it's stunning fantasy world and fluid and dynamic combat. I also did not have time to find a story and create UI elements but I find 
+that important as well to be able to make the game immersive.
+    
+*/
